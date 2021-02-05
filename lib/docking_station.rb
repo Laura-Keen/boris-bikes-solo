e894431fbdb936e7005f39a21c59b2165cb0117d
Laura <keenlaura@hotmail.co.uk>
2021-02-05 16:04:07 +0000

    Made no bikes error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,13 +3,23 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
+  def initialize
+    @bike = []
+  end 
+
   def release_bike
-    Bike.new
+    if bike.empty?
+      raise "No bikes available"
+    else
+      bike.pop
+    end
   end
 
   def dock(bike)
-    @bike = bike
+    self.bike << bike
   end
+
+ 
 
   # This method is no longer needed because of the attribute reader
   # def bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-
+  let(:bike) {Bike.new}
   #Long Syntax for respond_to test
   # it 'responds to release_bike' do
   #   expect(subject).to respond_to :release_bike
@@ -11,8 +11,8 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
   it 'releases working bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    subject.dock(bike)
+    expect(subject.release_bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).arguments }
@@ -20,17 +20,12 @@ describe DockingStation do
   it { is_expected.to respond_to(:bike)}
 
   it 'docks something' do
-    bike = Bike.new
     # return the bike we just docked
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
     subject.dock(bike)
-    # return the bike we just docked
-    expect(subject.bike).to eq bike
+    expect(subject.bike).to include(bike)
   end
 
-
+  it 'raises error when no bikes available' do
+    expect { subject.release_bike }.to raise_error
+  end
 end
